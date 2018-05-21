8eb84d645243febcb852abb606afec506c56495f
Ryan Lin <linhuiming@maoniunet.com>
2018-05-21 10:41:34 +0800

    构建环境

---
+++ rollup.config.js
@@ -0,0 +1,21 @@
+import json from 'rollup-plugin-json'
+import resolve from 'rollup-plugin-node-resolve'
+import babel from 'rollup-plugin-babel'
+
+export default {
+  input: 'src/main.js',
+  output: {
+    file: './dist/bundle.js',
+    name: 'bundle',
+    format: 'umd'
+  },
+  watch: {
+    include: 'src/**'
+  },
+  plugins: [ 
+    json(), 
+    resolve(),
+    babel({
+      exclude: 'node_modules/**' // 只编译我们的源代码
+    }) ]
+}
